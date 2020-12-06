70cdadb67e2b7013e5a4862c10ee072a87315271
Prashant Shinde <prashantns9@gmail.com>
2020-12-06 23:00:19 +0530

    chat box styling dong

--- realtime-chat-ui/src/components/chat/Chat.js
+++ realtime-chat-ui/src/components/chat/Chat.js
@@ -1,20 +1,24 @@
-import { useParams } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-import { useEffect, useState } from 'react';
-import io from 'socket.io-client';
-const SERVER_URI = 'http://localhost:5000';
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+import "./Chat.css";
+
+const SERVER_URI = "http://localhost:5000";
 let socket;
 
 function Chat() {
+  const [messagesEnd, setMessagesEnd] = useState(null);
   const { userName, roomName } = useParams();
   const [messages, setMessages] = useState([]);
-  const [userInput, setUserInput] = useState('');
+  const [userInput, setUserInput] = useState("");
   const history = useHistory();
 
   useEffect(() => {
     socket = io(SERVER_URI);
-    socket.emit('join', { userName, roomName }, (joined) => {
+    socket.emit("join", { userName, roomName }, (joined) => {
       if (!joined) {
         history.push("/");
       }
@@ -22,22 +26,25 @@ function Chat() {
     return () => {
       socket.disconnect();
       //socket.off();
-    }
+    };
   }, [userName, roomName]);
 
   useEffect(() => {
-    socket.on('message', message => {
+    socket.on("message", (message) => {
       console.log("Received message" + JSON.stringify(message));
       console.log("Previos msgs " + JSON.stringify(messages));
       setMessages([...messages, message]);
     });
+    if (messagesEnd) {
+      messagesEnd.scrollIntoView({ behavior: "smooth" });
+    }
     return () => {
       //setMessages([]);
-    }
+    };
   }, [messages]);
 
   useEffect(() => {
-    socket.on('error', error => {
+    socket.on("error", (error) => {
       alert(error.message);
     });
   });
@@ -45,16 +52,37 @@ function Chat() {
   function renderMessages() {
     let messageBoxes = [];
     for (let i = 0; i < messages.length; i++) {
-      messageBoxes.push(<li key={i}> {messages[i].from} - {messages[i].text} </li>)
+      messageBoxes.push(
+        <div className={`chat-bubble-container`} key={i}>
+          <label
+            className={`label ${
+              messages[i].from === userName ? "text-right" : "text-left"
+            }`}
+          >
+            {messages[i].from}
+          </label>
+          <div
+            className={`chat-bubble ${
+              messages[i].from === userName ? "sent" : "received"
+            } `}
+            key={i}
+          >
+            {messages[i].text}
+          </div>
+        </div>
+      );
     }
-    return <ul> {messageBoxes} </ul>;
+    return <div> {messageBoxes} </div>;
   }
 
   function sendMessage() {
     console.log("sending");
     setMessages([...messages, { from: userName, text: userInput }]);
-    socket.emit('sendMessage', { message: { from: userName, text: userInput }, roomName });
-    setUserInput('');
+    socket.emit("sendMessage", {
+      message: { from: userName, text: userInput },
+      roomName,
+    });
+    setUserInput("");
   }
 
   function leaveRoom() {
@@ -62,13 +90,52 @@ function Chat() {
   }
 
   return (
-    <div>
-      <h1> {roomName}</h1>
-      <button onClick={() => leaveRoom()}> Leave </button>
-      {renderMessages()}
-      <input value={userInput} placeholder="enter your message" onChange={(e) => setUserInput(e.target.value)} />
-
-      <button onClick={() => sendMessage()}> Send</button>
+    <div id="chat">
+      <div className="row header">
+        <div className="col-10">
+          <h4 id="room-name">{roomName}</h4>
+        </div>
+        <div className="col-2">
+          <div
+            id="leave-actn"
+            className="action text-right"
+            onClick={() => leaveRoom()}
+          >
+            leave
+          </div>
+        </div>
+      </div>
+      <div className="chat-box">
+        {renderMessages()}
+        <div
+          ref={(el) => {
+            setMessagesEnd(el);
+          }}
+        ></div>
+      </div>
+      <div className="user-input-container">
+        <div className="row">
+          <div className="col-10">
+            <input
+              id="user-input"
+              name="user-input"
+              value={userInput}
+              placeholder="Enter your message"
+              onChange={(e) => setUserInput(e.target.value)}
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-2 text-right">
+            <button
+              disabled={!userInput}
+              className={userInput ? "primary" : "gray"}
+              onClick={() => sendMessage()}
+            >
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- realtime-chat-ui/src/components/home/Home.js
+++ realtime-chat-ui/src/components/home/Home.js
@@ -1,37 +1,76 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 function Home() {
-  const [name, setName] = useState('');
-  const [roomName, setRoomName] = useState('');
+  const [name, setName] = useState("");
+  const [roomName, setRoomName] = useState("");
   return (
     <div id="home">
       <form>
-        <h2 id="app-name" className="primary"> Realtime Chat</h2>
+        <h2 id="app-name" className="primary">
+          {" "}
+          Realtime Chat
+        </h2>
         <div className="row">
           <div className="col-3 offset"> </div>
-          <div className="col-6 name-input-group" >
+          <div className="col-6 name-input-group">
             <label htmlFor="name">Your Name</label>
-            <input id="roomName" required name="name" value={name} type="text" placeholder="Ënter your name" onChange={(e) => setName(e.target.value)} /> <br />
+            <input
+              id="roomName"
+              required
+              name="name"
+              value={name}
+              type="text"
+              autoComplete="off"
+              placeholder="Ënter your name"
+              onChange={(e) => setName(e.target.value)}
+            />{" "}
+            <br />
           </div>
         </div>
         <div className="row">
           <div className="col-3 offset"> </div>
-          <div className="col-6 room-name-input-group" >
+          <div className="col-6 room-name-input-group">
             <label htmlFor="roomName">Room Name</label>
-            <input id="roomName" required name="roomName" value={roomName} type="text" placeholder="Ënter room name" onChange={(e) => setRoomName(e.target.value)} /> <br />
+            <input
+              id="roomName"
+              required
+              name="roomName"
+              value={roomName}
+              autoComplete="off"
+              type="text"
+              placeholder="Ënter room name"
+              onChange={(e) => setRoomName(e.target.value)}
+            />{" "}
+            <br />
           </div>
         </div>
         <div className="row">
           <div className="col-3 offset"> </div>
-          <div className="col-6 text-center" >
+          <div className="col-6 text-center">
             <Link to={`/chat/${roomName}/${name}`}>
-              <button type="submit" className="primary" disabled={!roomName || !name}> Enter Rooom </button>
+              <button
+                type="submit"
+                className="primary"
+                disabled={!roomName || !name}
+              >
+                {" "}
+                Enter Rooom{" "}
+              </button>
             </Link>
           </div>
         </div>
       </form>
-      <footer><div className="footer-para"> Made with &#128151; by <a href="https://www.prashantshinde.in" target="_new"> Prashant </a> </div> </footer>
+      <footer>
+        <div className="footer-para">
+          {" "}
+          Made with &#128151; by{" "}
+          <a href="https://www.prashantshinde.in" target="_new">
+            {" "}
+            Prashant{" "}
+          </a>{" "}
+        </div>{" "}
+      </footer>
     </div>
   );
 }
