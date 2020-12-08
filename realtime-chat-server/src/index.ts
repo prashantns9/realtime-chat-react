02fbc2147ffe5e3afe9100813ff1f0d5b41f70b4
Prashant Shinde <prashantns9@gmail.com>
2020-12-08 19:45:19 +0530

    serevr to serve static files

--- realtime-chat-server/src/index.ts
+++ realtime-chat-server/src/index.ts
@@ -1,33 +1,32 @@
-import ChatApp from './ChatApp';
-import express from 'express';
-import {Server, Socket } from 'socket.io';
-import http from 'http';
-import routes from './routes'
+import ChatApp from "./ChatApp";
+import express from "express";
+import { Server, Socket } from "socket.io";
+import http from "http";
+import routes from "./routes";
+import path from "path";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 var CORS_OPTIONS;
 
-
-if (process.env.NODE_ENV !== 'production') {
-    CORS_OPTIONS = {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+if (process.env.NODE_ENV !== "production") {
+  CORS_OPTIONS = {
+    origin: "*",
+    methods: ["GET", "POST"],
+  };
 }
-
+app.use(express.static(path.join(__dirname, "./../public")));
 app.use(routes);
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: CORS_OPTIONS
+  cors: CORS_OPTIONS,
 });
 
 const chatApp = new ChatApp(io);
 
 server.listen(PORT, () => {
-    console.log("Server is listening on " + PORT);
+  console.log("Server is listening on " + PORT);
 });
-
--- realtime-chat-server/src/routes.ts
+++ realtime-chat-server/src/routes.ts
@@ -1,9 +1,13 @@
-import express from 'express';
+import express from "express";
+import path from "path";
 
 const router: express.Router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('App is running');
+router.get("/api", (req, res) => {
+  res.send("App is running");
 });
 
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./../public", "index.html"));
+});
 export default router;
